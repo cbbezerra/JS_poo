f5d44cdf1eb4c7c1b95c5f3d6280041f9c5d4821
cbbezerra <carlafbelfort@gmail.com>
2021-11-03 18:09:39 -0300

    composição de classes

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-class Cliente {
-    nome;
-    cpf;
-   }
-
-class ContaCorrente {
-    agencia;
-    saldo;
-
-    sacar(valor){
-        if(this.saldo >= valor){
-            this.saldo -= valor;
-        }
-    }
-}
-
-
+import { Cliente } from "./cliente.js";
+import {ContaCorrente} from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Carla";
@@ -29,22 +14,25 @@ cliente3.nome = "Dandara";
 cliente3.cpf = "99988877752";
 
 const contaCorrenteCarla = new ContaCorrente();
-contaCorrenteCarla.saldo = 0;
 contaCorrenteCarla.agencia = 1001;
+contaCorrenteCarla.cliente = cliente1;
+contaCorrenteCarla.depositar(200);
 
-console.log(contaCorrenteCarla.saldo);
-contaCorrenteCarla.saldo = 100;
-console.log(contaCorrenteCarla.saldo);
-contaCorrenteCarla.sacar(250);
+console.log(contaCorrenteCarla);
+
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
+console.log(conta2);
+
+const valorSacado = contaCorrenteCarla.sacar(50);
+console.log(valorSacado);
+
+contaCorrenteCarla.transferir(100, conta2);
+
+console.log(conta2);
+console.log(contaCorrenteCarla);
 
 
 
-
-
-
-console.log(contaCorrenteCarla.saldo);
-
-
-
-console.log(cliente1);
 
